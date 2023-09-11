cd2f7496cd2a20b02578511c8db234167428343e
daniel-ferreira-da-costa <danielferreira@unitins.br>
2023-09-11 01:41:34 -0300

    Enum TamanhoNarguile Finalizado

--- src/main/java/unitins/model/TamanhoNarguile.java
+++ src/main/java/unitins/model/TamanhoNarguile.java
@@ -1,5 +1,23 @@
 package unitins.model;
 
 public enum TamanhoNarguile {
-    
+    GRANDE(1, "Grande"),
+    MEDIO(2, "Medio"),
+    PEQUENO(3, "Pequeno");
+
+    private final int id;
+    private final String descricao;
+
+    private TamanhoNarguile(int id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
